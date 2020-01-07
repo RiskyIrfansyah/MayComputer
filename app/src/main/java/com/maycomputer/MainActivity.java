6060fbed278268840681cb315d50fcd38a180c43
Muhamad Rizqi Ramdani <rizqiramdanigit@gmail.com>
2020-01-07 18:25:46 +0700

    tinggal your bild

--- app/src/main/java/com/maycomputer/MainActivity.java
+++ app/src/main/java/com/maycomputer/MainActivity.java
@@ -3,6 +3,7 @@ package com.maycomputer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,12 +11,28 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     private Switch mySwitch;
+    private Button btnBuild, btnHasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mySwitch = (Switch) findViewById(R.id.mySwitch);
+
+        //button Build
+        btnBuild = (Button) findViewById(R.id.build);
+
+        //button hasil
+
+        btnBuild.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iBuild = new Intent(getApplicationContext(), Build.class);
+                startActivity(iBuild);
+            }
+        });
+
+
 
     }
 
@@ -31,4 +48,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, about.class);
         startActivity(intent);
     }
+
+
+
+
+
 }
